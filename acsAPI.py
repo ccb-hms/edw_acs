d8dbff3ffa810e70d295f44d3c90d08d01aa4caf
sam-pullman <101196589+sam-pullman@users.noreply.github.com>
2022-04-22 16:18:26 -0400

    Update acsAPI.py

--- acsAPI.py
+++ acsAPI.py
@@ -11,8 +11,26 @@ from lxml import etree
 from io import StringIO
 import pyodbc
 from itertools import product
+import argparse
+import sys
+import logging
+import logging.config
+import csv
+import os
 
-def find_tables(year_range, start="B01001", multi=True):
+def find_tables(uid, pwd, ipaddress):
+    # Connect to the master db and create the db for future use 
+    driver = "ODBC Driver 17 for SQL Server"
+    conn = pyodbc.connect(f"DRIVER={driver};SERVER={ipaddress};DATABASE=master;UID={uid};PWD={pwd}", autocommit=True)
+    cursor = conn.cursor()
+    drop_create_db = '''USE master;
+                    ALTER DATABASE [AmericanCommunitySurvey] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                    DROP DATABASE [AmericanCommunitySurvey] ;
+                    CREATE DATABASE [AmericanCommunitySurvey] ;'''
+    
+    cursor.execute(drop_create_db)
+    conn.commit()
+
     # Send an http request to the census website to collect all available table shells
     html_parser = etree.HTMLParser()
     web_page = requests.get('https://www.census.gov/programs-surveys/acs/technical-documentation/table-shells.2019.html', timeout=10)
@@ -21,10 +39,24 @@ def find_tables(year_range, start="B01001", multi=True):
     dom_tree = etree.parse(str_io_obj, parser=html_parser)
     link = dom_tree.xpath('//*[@name="2019 ACS Table List"]/@href')[0]
 
-    # Use pandas(pd) to read the csv file into a dataframe
+    #connect to the AmericanCommunitySurvey db
+    conn = pyodbc.connect(f"DRIVER={driver};SERVER={ipaddress};DATABASE=AmericanCommunitySurvey;UID={uid};PWD={pwd}", autocommit=True)
+    cursor = conn.cursor()
+
+    # Use pandas(pd) to read the csv file of ACS tablenamesinto a dataframe
     table_lst = pd.read_excel(link, engine='openpyxl')
 
-    # Transform the dataframe into a dictionary 
+    # Export the csv to sql as a table legend
+    legend = table_lst.to_csv('/HostData/Legend.csv', sep=',', encoding='utf-8', index=False)
+    create = pd.io.sql.get_schema(table_lst, 'TableLegend')
+    cursor.execute(create)
+    conn.commit()
+
+    bulk_insert = "BULK INSERT TableLegend FROM '" + '/HostData/Legend.csv' + "' WITH (TABLOCK, FIELDTERMINATOR = ',',ROWTERMINATOR = '\n');"
+    cursor.execute(bulk_insert)
+    conn.commit()
+
+    # Transform the table_lst dataframe into a dictionary 
     table_lst = table_lst.to_dict("records")
 
     # The table_lst dictionary is a dict of ALL tables, but we only want the Base detailed tables, 
@@ -37,9 +69,10 @@ def find_tables(year_range, start="B01001", multi=True):
         if i['Table ID'][0] == "B": 
             tables[i['Table ID']] = i['Table Title']
 
-    get_acs_data(tables, year_range, start, multi)
+    get_acs_data(tables, years=args.year, start=args.start, alone=args.alone)
 
-def get_acs_data(tables, years, start="B01001", multi=True):
+def get_acs_data(tables, years, start, alone):
+    logger = logging.getLogger('api_logger')
     # If the user entered a specific table (optional arg), filter out the one's we've already done. 
     filtered_tables = list(tables.keys())[list(tables.keys()).index(start):]
 
@@ -54,18 +87,14 @@ def get_acs_data(tables, years, start="B01001", multi=True):
         year2 = int(years)+1
 
     # Loop through each year in the users defined range, and each table available in the API
-    failed_apis = open("/HostData/failed_api_calls.txt", "a")
-
     for year, table in product(range(year1, year2), tables):
         if table in filtered_tables:
-            if multi == False:
-                table = start
             # API request to get all zipcode tabulated data for the current year and table
             print(f"{year} - {table}")
             try:
-                response = requests.get(f'https://api.census.gov/data/{year}/acs/acs5?get=NAME,group({table})&for=zip%20code%20tabulation%20area:*&key=62fade369e5f8276f58c592eed6a5a6e19bdbb3a',timeout=10)            
+                response = requests.get(f'https://api.census.gov/data/{year}/acs/acs5?get=NAME,group({table})&for=zip%20code%20tabulation%20area:*&key=62fade369e5f8276f58c592eed6a5a6e19bdbb3a',timeout=100)            
                 if response.status_code != 200:
-                    failed_apis.write(f"https://api.census.gov/data/{year}/acs/acs5?get=NAME,group({table})&for=zip%20code%20tabulation%20area:*&key=62fade369e5f8276f58c592eed6a5a6e19bdbb3a\n")
+                    logger.warning(f'https://api.census.gov/data/{year}/acs/acs5?get=NAME,group({table})&for=zip%20code%20tabulation%20area:*&key=62fade369e5f8276f58c592eed6a5a6e19bdbb3a')
                     pass
                 else:
                     # If the API call returns data, load it as a json, then use pandas to transform the data into a dataframe
@@ -103,23 +132,28 @@ def get_acs_data(tables, years, start="B01001", multi=True):
                     filename = 'ACS_5Y_Estimates_{year}_{table}'.format(year=year,table=table)
                     filepath = path + filename + ".txt"
                     df.to_csv(filepath, encoding='utf-8', index=False, sep =',')
-                    acs_ETL(df, filename, filepath)
-                    if multi == False:
-                        exit
+                    acs_ETL(df, filename, filepath, uid=args.uid, pwd=args.pwd, ipaddress=args.ipaddress)
+
             except Exception as e:
-                failed_get_acs_data = open("/HostData/failed_get_acs_data.txt", "a")
-                failed_get_acs_data.write(\n filename)
-                failed_get_acs_data.write('\n ERROR - %s' % e)
-                failed_get_acs_data.close()
+                logger.warning(e)
 
-def acs_ETL(df, filename, filepath):
+            # Check if this is supposed to be a one-off table pull or not
+            if not alone:
+                break
 
-    # Insert whole DataFrame into MySQL
-    conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=172.17.0.2, 1433;DATABASE=master;UID=sa;PWD=<YourStrong@Passw0rd>", autocommit=True)
+
+def acs_ETL(df, tablename, filepath, uid, pwd, ipaddress):
+    # Logging
+    logger = logging.getLogger('sql_logger')
+    # Connect into DB Server
+    driver = "ODBC Driver 17 for SQL Server"
+    conn = pyodbc.connect(f"DRIVER={driver};SERVER={ipaddress};DATABASE=AmericanCommunitySurvey;UID={uid};PWD={pwd}", autocommit=True)
     cursor = conn.cursor()
 
+    # NPCOMMENT: all of the columns are showing up as nvarchar(max), even the ones
+    # with numeric data
     # Create schema, edit text fields to nvarchar
-    create = pd.io.sql.get_schema(df, filename)
+    create = pd.io.sql.get_schema(df, tablename)
     create = create.replace("TEXT", "NVARCHAR(MAX)")
 
     # Execute table creation and bulk insert
@@ -127,24 +161,71 @@ def acs_ETL(df, filename, filepath):
         cursor.execute(create)
         conn.commit()
 
-        bulk_insert = "BULK INSERT " + filename + " FROM '" + filepath + "' WITH (TABLOCK, FIRSTROW=2, FIELDTERMINATOR = ',',ROWTERMINATOR = '\n');"
+        bulk_insert = "BULK INSERT " + tablename + " FROM '" + filepath + "' WITH (TABLOCK, FIRSTROW=2, FIELDTERMINATOR = ',',ROWTERMINATOR = '\n');"
         cursor.execute(bulk_insert)
         conn.commit()
 
     except Exception as e:
-        failed_sql = open("/HostData/failed_sql.txt", "a")
-        failed_sql.write('\n\n ERROR - %s' % e)
-        failed_sql.close()
+        logger.warning(e)
 
 if __name__ == "__main__":
-    year_range = str(sys.argv[1])
+    # Construct the argument parser
+    parser = argparse.ArgumentParser()
+
+    # Add the arguments to the parser
+    parser.add_argument('-y', '--year', type= str, required=True, action="store", help='the year (format YYYY) or years (format YYYY-YYYY) you would like to pull data for. This should be a str.')
+    parser.add_argument('-s', '--start', type= str, required=False, action="store", default = 'B01001', help='If you would like to pull a single table, or start the pull from a specific table, define it here as a string, ex. "B01001".')
+    parser.add_argument('-a', '--alone', required=False, action="store_false", help='If you would like to pull a single table, use this option.')
+    parser.add_argument('-u', '--uid', type= str, required=True, action="store", help='User ID for the DB server')
+    parser.add_argument('-p', '--pwd', type= str, required=True, action="store", help='Password for the DB server')
+    parser.add_argument('-ip', '--ipaddress', type= str, required=True, action="store", help='The network address of the DB server')    
+
+    if len(sys.argv) < 2:
+        parser.print_help()
+        parser.print_usage()
+        parser.exit()
+        
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='/HostData/logging.log',level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+    logging.config.dictConfig({
+        'version': 1,
+        'formatters': {
+            'simple_formatter': {
+                'format': '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+            }
+        },
+        'handlers': {
+            'api_calls': {
+                'class' : 'logging.FileHandler',
+                'formatter': 'simple_formatter',
+                'filename': '/HostData/api.txt'
+            },
+            'sql_calls': {
+                'class' : 'logging.FileHandler',
+                'formatter': 'simple_formatter',
+                'filename': '/HostData/sql.txt'
+            }
+        },
+        'loggers': {
+            'api_logger': {
+                'handlers': ['api_calls']
+            },
+            'sql_logger': {
+                'handlers': ['sql_calls']
+            }
+        }
+    })
+    logging.info(f'Starting data pull for {args.year}')
     
-    if len(sys.argv) > 2:
-        start = str(sys.argv[2])
-        if len(sys.argv) > 3:
-            multi = str(sys.argv[3])
-            find_tables(year_range, start, multi)
-        else:
-            find_tables(year_range, start)
-    else:
-        find_tables(year_range)
+    find_tables(uid=args.uid, pwd=args.pwd, ipaddress=args.ipaddress)
+    
+    with open('/HostData/logging.log', 'r') as logfile, open('/HostData/LOGFILE.csv', 'w') as csvfile:
+        reader = csv.reader(logfile, delimiter='|')
+        writer = csv.writer(csvfile, delimiter=',',)
+        writer.writerow(['EventTime', 'Origin', 'Level', 'Message'])
+        writer.writerows(reader)
+    
+    os.remove('/HostData/sql.txt')
+    os.remove('/HostData/api.txt')
